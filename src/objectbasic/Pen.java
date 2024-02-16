1df77d4f1534b5482c5ceb865c3967a900e80e15
Pratistha Shrestha <pratishresthatech@gmail.com>
2024-02-15 20:21:13 -0600

    updated data types in all classes

--- src/objectbasic/Pen.java
+++ src/objectbasic/Pen.java
@@ -3,12 +3,11 @@ package objectbasic;
 public class Pen {
     //States
 
-
     /*
     Brand Name - String
     Price - float
     Color - String
-    Type of stroke weight - float
+    Type of stroke weight - String
     Type of pen (gel pen, ballpoint, roller ball) - String
     Type of body (metal or plastic) - String
     Has click-top - boolean
