77cab7c0b021d21a9047e5d3d7883038c517addd
Pratistha Shrestha <pratishresthatech@gmail.com>
2024-02-21 20:11:23 -0600

    updated the states to be private in Category class.

--- src/himalayanTimes/Category.java
+++ src/himalayanTimes/Category.java
@@ -1,6 +1,7 @@
 package himalayanTimes;
 
 public class Category {
+
     //States
     /*
         Category Id - int
@@ -11,15 +12,71 @@ public class Category {
         Created At - String
         Updated At - String
     */
-        int categoryId;
-        String categoryName;
-        boolean isDisabled;
-        int order;
-        boolean isDeleted;
-        String createdAt;
-        String updatedAt;
+        private int categoryId;
+        private String categoryName;
+        private boolean isDisabled;
+        private int order;
+        private boolean isDeleted;
+        private String createdAt;
+        private String updatedAt;
+    public int getCategoryId() {
+        return categoryId;
+    }
 
-        @Override
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public boolean isDisabled() {
+        return isDisabled;
+    }
+
+    public void setDisabled(boolean disabled) {
+        isDisabled = disabled;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+
+    @Override
         public String toString() {
             return "Category{" +
                     "categoryId=" + categoryId +
--- src/himalayanTimes/HimalayanTimesApplication.java
+++ src/himalayanTimes/HimalayanTimesApplication.java
@@ -4,155 +4,19 @@ public class HimalayanTimesApplication {
     public static void main (String[]args){
 
         Category home = new Category();
-        home.categoryId = 1;
-        home.categoryName = "Home";
-        home.order = 1;
-        home.isDeleted = false;
-        home.isDisabled = false;
-        home.createdAt = "2024-02-16";
-        home.updatedAt = "2024-02-16";
-        System.out.println(home);
-
         Category kathmandu = new Category();
-        kathmandu.categoryId = 2;
-        kathmandu.categoryName = "Kathmandu";
-        kathmandu.order = 2;
-        kathmandu.isDeleted = false;
-        kathmandu.isDisabled = false;
-        kathmandu.createdAt = "2024-02-16";
-        kathmandu.updatedAt = "2024-02-16";
-        System.out.println(kathmandu);
-
         Category nepal = new Category();
-        nepal.categoryId = 3;
-        nepal.categoryName = "Nepal";
-        nepal.order = 3;
-        nepal.isDeleted = false;
-        nepal.isDisabled = false;
-        nepal.createdAt = "2024-02-16";
-        nepal.updatedAt = "2024-02-16";
-        System.out.println(nepal);
-
-
         Category covid19 = new Category();
-        covid19.categoryId = 4;
-        covid19.categoryName = "Covid-19";
-        covid19.order = 4;
-        covid19.isDeleted = false;
-        covid19.isDisabled = false;
-        covid19.createdAt = "2024-02-16";
-        covid19.updatedAt = "2024-02-16";
-        System.out.println(covid19);
-
         Category covidConnect = new Category();
-        covidConnect.categoryId = 5;
-        covidConnect.categoryName = "Covid Connect";
-        covidConnect.order = 5;
-        covidConnect.isDeleted = false;
-        covidConnect.isDisabled = false;
-        covidConnect.createdAt = "2024-02-16";
-        covidConnect.updatedAt = "2024-02-16";
-        System.out.println(covidConnect);
-
         Category world = new Category();
-        world.categoryId = 6;
-        world.categoryName = "World";
-        world.order = 6;
-        world.isDeleted = false;
-        world.isDisabled = false;
-        world.createdAt = "2024-02-16";
-        world.updatedAt = "2024-02-16";
-        System.out.println(world);
-
-
         Category opinion = new Category();
-        opinion.categoryId = 7;
-        opinion.categoryName = "Opinion";
-        opinion.order = 7;
-        opinion.isDeleted = false;
-        opinion.isDisabled = false;
-        opinion.createdAt = "2024-02-16";
-        opinion.updatedAt = "2024-02-16";
-        System.out.println(opinion);
-
         Category business = new Category();
-        business.categoryId = 8;
-        business.categoryName = "Business";
-        business.order = 8;
-        business.isDeleted = false;
-        business.isDisabled = false;
-        business.createdAt = "2024-02-16";
-        business.updatedAt = "2024-02-16";
-        System.out.println(business);
-
         Category sports = new Category();
-        sports.categoryId = 9;
-        sports.categoryName = "Sports";
-        sports.order = 9;
-        sports.isDeleted = false;
-        sports.isDisabled = false;
-        sports.createdAt = "2024-02-16";
-        sports.updatedAt = "2024-02-16";
-        System.out.println(sports);
-
         Category entertainment = new Category();
-        entertainment.categoryId = 10;
-        entertainment.categoryName = "Entertainment";
-        entertainment.order = 10;
-        entertainment.isDeleted = false;
-        entertainment.isDisabled = false;
-        entertainment.createdAt = "2024-02-16";
-        entertainment.updatedAt = "2024-02-16";
-        System.out.println(entertainment);
-
         Category lifestyle = new Category();
-        lifestyle.categoryId = 11;
-        lifestyle.categoryName = "Lifestyle";
-        lifestyle.order = 11;
-        lifestyle.isDeleted = false;
-        lifestyle.isDisabled = false;
-        lifestyle.createdAt = "2024-02-16";
-        lifestyle.updatedAt = "2024-02-16";
-        System.out.println(lifestyle);
-
         Category scienceTech = new Category();
-        scienceTech.categoryId = 12;
-        scienceTech.categoryName = "Science & Tech";
-        scienceTech.order = 12;
-        scienceTech.isDeleted = false;
-        scienceTech.isDisabled = false;
-        scienceTech.createdAt = "2024-02-16";
-        scienceTech.updatedAt = "2024-02-16";
-        System.out.println(scienceTech);
-
         Category blog = new Category();
-        blog.categoryId = 13;
-        blog.categoryName = "Blog";
-        blog.order = 13;
-        blog.isDeleted = false;
-        blog.isDisabled = false;
-        blog.createdAt = "2024-02-16";
-        blog.updatedAt = "2024-02-16";
-        System.out.println(blog);
-
         Category environment = new Category();
-        environment.categoryId = 14;
-        environment.categoryName = "Environment";
-        environment.order = 14;
-        environment.isDeleted = false;
-        environment.isDisabled = false;
-        environment.createdAt = "2024-02-16";
-        environment.updatedAt = "2024-02-16";
-        System.out.println(environment);
-
         Category health = new Category();
-        health.categoryId = 15;
-        health.categoryName = "Health";
-        health.order = 15;
-        health.isDeleted = false;
-        health.isDisabled = false;
-        health.createdAt = "2024-02-16";
-        health.updatedAt = "2024-02-16";
-        System.out.println(health);
     }
 }
