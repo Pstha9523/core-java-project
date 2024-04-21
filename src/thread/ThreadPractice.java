2fd4f29324164a19de227a65c154becb50bd9880
Pratistha Shrestha <pratishresthatech@gmail.com>
2024-04-21 13:30:54 -0500

    added ThreadPractice class.

---
+++ src/thread/ThreadPractice.java
@@ -0,0 +1,38 @@
+package thread;
+
+public class ThreadPractice {
+
+
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+
+            }
+        });
+        Thread t3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+    }
+
+
+
+}
